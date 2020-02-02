92b4cbb621ef58301464cc44b63801898e0f1b9f
Emilio Araos <ea3dev@gmail.com>
2020-02-02 05:34:27 -0700

    fixed bug

--- scope.py
+++ scope.py
@@ -16,5 +16,30 @@ def fact(n):
         return n * fact(n - 1)
 
 
-for i in range(130):
-    print(i, fact(i))
+def fib(n):
+    """F(n) = F(n - 1) + F(n - 2)"""
+    if n < 2:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def fibonacci(n):
+    if n == 0:
+        result = 0
+    elif n == 1:
+        result = 1
+    else:
+        n_minus1 = 1
+        n_minus2 = 0
+        for f in range(1, n):
+            result = n_minus2 + n_minus1
+            n_minus2 = n_minus1
+            n_minus1 = result
+    return result
+
+
+for i in range(35):
+    print(i, fibonacci(i))
+
+
